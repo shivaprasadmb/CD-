0e42d00b1fc46c4472b8ec598514674b13319b62
TrafalgarLaw28 <60063748+TrafalgarLaw28@users.noreply.github.com>
2020-06-12 07:05:59 +0530

    Added comments

--- LeftRecursion.py
+++ LeftRecursion.py
@@ -5,12 +5,13 @@ while(1):
         p.append(n)
     else:
         break
-    
+#p is a list used to store all grammar rules entered one rule at a time. Use "null" to represent null instead of using lambda or epsilon.    
 q = list()
 for i in p:
     if (i[1:3]=="->") and (i[0]==i[3]):
         if not (i[0] in q):
             q.append(i[0])
+#q is a list used to store all the non terminals that have a rule with left recursion. This program can only work for CFG.
 r = list()
 for i in p:
     if i[0] in q:
@@ -28,6 +29,7 @@ for i in p:
             r.append(y)
     else:
         r.append(i)
+#r is a list used to store all the grammar rules after left recursion is removed.
 print("\nLeft recursion removed grammar is \n")
 for i in r:
     print(i)
