881df1417af1fe77358ca3597cecf85dd3771afd
ShivaprasadMB <smbenal99@gmail.com>
2020-06-12 17:00:11 +0530

    renamed the file name

---
+++ LeftRecursion2.py
@@ -0,0 +1,45 @@
+grammer = list()
+i = 0
+print("enter your grammer rules, enter \'exec\' when completed\n")
+while(True):
+    rule = input("enter " + str(i)+"th rule\n")
+    if(rule != "exec"):
+        grammer.append(rule)
+        i = i + 1
+    else:
+        break
+print()
+new_grammer = list()
+for j in grammer:
+    left = j[0]
+    ext = list()
+    t = j[3:].split('|')
+    for k in t:
+        ext.append(k)
+    LR = False
+    for q in ext:
+        if(q[0] == left):
+            LR = True  # left recursion is there
+    if(LR == False):
+        new_grammer.append(left+"->")
+        for q in ext:
+            new_grammer.append(q+"|")
+        new_grammer[-1] = new_grammer[-1][:-1]
+    else:
+        new_grammer.append(left+"->")
+        for q in ext:
+            if(q[0] != left):
+                new_grammer.append(q+left+"\'|")
+        new_grammer[-1] = new_grammer[-1][:-1]
+        new_grammer.append("_"+left+"\'->")
+        for q in ext:
+            if(q[0] == left):
+                new_grammer.append(q[1:]+left+"\'|")
+        new_grammer[-1] = new_grammer[-1][:-1]
+        new_grammer.append("|null_")
+
+new_grammer = [''.join(new_grammer)]
+final_grammer = new_grammer[0].split('_')
+print("final grammer after left recursion removal\n")
+for p in final_grammer:
+    print(p)
