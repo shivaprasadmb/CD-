c97aec3177feb654bc7e6abb39ad0ee0c89fffcf
TrafalgarLaw28 <60063748+TrafalgarLaw28@users.noreply.github.com>
2020-06-12 15:37:06 +0530

    Update LeftRecursion.py

--- LeftRecursion.py
+++ LeftRecursion.py
@@ -1,46 +1,35 @@
-grammer = list()
-i = 0
-print("enter your grammer rules, enter \'exec\' when completed\n")
-while(True):
-    rule = input("enter " + str(i)+"th rule\n")
-    if(rule != "exec"):
-        grammer.append(rule)
-        i = i + 1
+p = list()
+while(1):
+    n = input("Enter grammar production rule(Enter \'done\' when completed) ")
+    if (n!="done"):
+        p.append(n)
     else:
         break
-#p is a list used to store all grammar rules entered one rule at a time. Use "null" to represent null instead of using lambda or epsilon. 
-print()
-new_grammer = list()
-for j in grammer:
-    left = j[0]
-    ext = list()
-    t = j[3:].split('|')
-    for k in t:
-        ext.append(k)
-    LR = False
-    for q in ext:
-        if(q[0] == left):
-            LR = True  # left recursion is there
-    if(LR == False):
-        new_grammer.append(left+"->")
-        for q in ext:
-            new_grammer.append(q+"|")
-        new_grammer[-1] = new_grammer[-1][:-1]
+#p is a list used to store all grammar rules entered one rule at a time. Use "null" to represent null instead of using lambda or epsilon.    
+q = list()
+for i in p:
+    if (i[1:3]=="->") and (i[0]==i[3]):
+        if not (i[0] in q):
+            q.append(i[0])
+#q is a list used to store all the non terminals that have a rule with left recursion. This program can only work for CFG.
+r = list()
+for i in p:
+    if i[0] in q:
+        if i[3:] == "null":
+            y = str(i[0:3])+str(i[0])+"\'"
+            r.append(y)
+        elif i[0]!=i[3]:
+            y = (i+i[0]+'\'')
+            r.append(y)
+            y = str(i[0])+"\'"+"->null"
+            if not (y in r):
+                r.append(y)
+        elif i[0] == i[3]:
+            y = str(i[0])+"\'->"+i[4:]+str(i[0])+"\'"
+            r.append(y)
     else:
-        new_grammer.append(left+"->")
-        for q in ext:
-            if(q[0] != left):
-                new_grammer.append(q+left+"\'|")
-        new_grammer[-1] = new_grammer[-1][:-1]
-        new_grammer.append("_"+left+"\'->")
-        for q in ext:
-            if(q[0] == left):
-                new_grammer.append(q[1:]+left+"\'|")
-        new_grammer[-1] = new_grammer[-1][:-1]
-        new_grammer.append("|null_")
-
-new_grammer = [''.join(new_grammer)]
-final_grammer = new_grammer[0].split('_')
-print("final grammer after left recursion removal\n")
-for p in final_grammer:
-    print(p)
+        r.append(i)
+#r is a list used to store all the grammar rules after left recursion is removed.
+print("\nLeft recursion removed grammar is \n")
+for i in r:
+    print(i)
